8cec3263077b561aa0adc73474abb77fb4e2e046
Rawezh Hama <rawezhhame@gmail.com>
2022-02-15 01:32:47 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'app/modules/home/view.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'ToDo App',
+      home: HomePage(),
+    );
+  }
+}
+
